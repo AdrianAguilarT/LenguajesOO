736bc4621fe982bca108cc658b08b81e58978f5a
AdrianAguilarT <31869875+AdrianAguilarT@users.noreply.github.com>
2017-11-27 19:42:50 -0600

    Add files via upload

--- lenguajes/src/conexion/ProbarConexion.java
+++ lenguajes/src/conexion/ProbarConexion.java
@@ -1,25 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conexion;
 import java.sql.*;
-/**
- *
- * @author T-102
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ProbarConexion {
     public static void main(String [] args){
         Connection con = null;
         try{
-        con = Conexion.Conectarse("root", "");
+            con = Conexion.Conectarse("root", "");
+            System.out.println("Conexión Realizada");
         }catch(ClassNotFoundException e){
-            
+            System.out.println("No se cargó bien el driver");
         }catch(SQLException e){
-            
+            System.out.println("Un error de SQL "+e.getMessage());
         }finally{
-            con.close();
+            try {
+                if(con != null)
+                    con.close();
+                    System.out.println("Cerrado");
+            } catch (SQLException ex) {
+                
+            }
         }
     }
 }
